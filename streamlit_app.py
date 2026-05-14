c30f3e793af1dfcab7654c8ab71eb6c01abd0868
Arturo-5070 <sotovazar@gmail.com>
2026-05-14 21:27:32 +0000

    release version 1.0

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from google.cloud import firestore
 from google.oauth2 import service_account
 import json
 
-key_dict = json.loads((st.secrets["FIREBASE_MOVIES"]))
+key_dict = json.loads((st.secrets["textkey"]))
 creds = service_account.Credentials.from_service_account_info(key_dict)
 db = firestore.Client(credentials=creds, project="dashboardmovies-arturosoto")
 dbNames = db.collection("movies")
